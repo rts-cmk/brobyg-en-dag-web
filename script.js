a2d807ee8b394b4ab9f5daf30d2b506154ebc398
akaLaws <sel@rts.dk>
2022-10-26 14:11:08 +0200

    added assignment description & popup example

---
+++ script.js
@@ -0,0 +1,14 @@
+const popButton = document.querySelector("[name='pop']");
+const theImg = document.querySelector(".hoverimg");
+const closeButton = document.querySelector("[name='close'");
+
+
+const showImg = () => {
+
+    theImg.style.display = "flex";
+}
+
+const closeImg = () => {
+
+    theImg.style.display = "none";
+}
